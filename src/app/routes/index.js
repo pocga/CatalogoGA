8099f7327f53a4661ef6bae8445110c0877aa8e0
Sebastian Alarcon <sebastianalarcon@sophossolutions.com>
2019-03-27 16:25:33 -0500

    Commit inicial de rama Development, carga de archivos

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -31,10 +31,28 @@ var catalogo = [{
     "miniatura": ""
 },
 {
-    "idProducto": "000302",
+    "idProducto": "003",
     "categoria": "tv",
     "cantidadDisponible": 20,
-    "precio": 5000,
+    "precio": 4000,
+    "descripcion": "super bbb",
+    "imagen": "",
+    "miniatura": ""
+},
+{
+    "idProducto": "004",
+    "categoria": "tv",
+    "cantidadDisponible": 20,
+    "precio": 25000,
+    "descripcion": "super bbb",
+    "imagen": "",
+    "miniatura": ""
+},
+{
+    "idProducto": "005",
+    "categoria": "tv",
+    "cantidadDisponible": 20,
+    "precio": 2000,
     "descripcion": "super bbb",
     "imagen": "",
     "miniatura": ""
@@ -49,7 +67,7 @@ var catalogo = [{
 router.get('/catalogo/productos', async (req, res) => {
     console.log("file");
 
-    var test = await  fetch('http://localhost:8080/Inventario/getCategoriaAll').then(function(response) {
+    /*var test = await  fetch('http://localhost:8080/Inventario/getProductoAll').then(function(response) {
         return response.json();
         })
         .then(function(myJson) {
@@ -61,11 +79,48 @@ router.get('/catalogo/productos', async (req, res) => {
 
     console.log("Partamos de aca")
     console.log(test);
-    console.log("quiza")
+    console.log("quiza")*/
 
+    
+
+
+    
     try{
+        //const productos = await producto.find({});        
+
         const productos = await producto.find({});        
-        res.json(productos);
+        
+        let products = [];
+        catalogo.forEach(element => {
+            const productFound = productos.filter(value => value.idProducto === element.idProducto)
+            if(productFound.length > 0) {
+                element.imagen = productFound[0].imagen;
+                element.miniatura = productFound[0].miniatura;
+                products.push(...catalogo)
+            }
+
+        })
+
+
+
+        /*      
+        let products = [];
+        catalogo.forEach(element => {
+            productos = array2.filter(value => value.idProducto === element.idProducto)
+            if(productos.length > 0) {
+                element.imagen = productos[0].imagen;
+                element.miniatura = productos[0].miniatura;
+                products.push(...element)
+            }
+        })
+        if(products.length > 0 ) {
+            console.log(products)
+        }else {
+            console.log('No hay productos para mostrar')
+        }*/
+
+
+        res.json(catalogo);
         //console.log(myvariable + "funciono");
     } catch (error) {
         res.send(error);
@@ -77,17 +132,44 @@ router.get('/catalogo/productos', async (req, res) => {
 //localhost:4000/producto/categoria/""""
 router.get('/catalogo/productos/categorias', async (req, res) => {
     console.log('its me again');
+       
     try{
         var cate = [];
-         
-        for (i = 0; i < catalogo.length; i++){
-            cate[i] = catalogo[i].categoria;
-        }
-        console.log(cate);
-        let unique = [...new Set(cate)];
-        console.log(unique);
-        res.json(unique);
-        }
+        const test = await  fetch('http://localhost:8080/Inventario/getProductoAll').then(function(response) {
+            return response.json();
+        })
+        .then(response=>{
+            response.producto.forEach(element=>{
+                cate += element.categoria + ',';
+            })
+            console.log('CATE=', cate);
+            return cate;
+        })
+
+        .catch(function(err){
+            console.log(err);
+        });
+
+
+            //const cityJobsData = fetch('http://api.open-notify.org/astros.json');
+            //cityJobsData
+            //.then(data => data.json())
+            //.catch(err => console.log(err));
+
+         //console.log(test);
+         /*
+        catalogo.forEach(element =>{
+                cate.push(element.categoria);
+                //console.log('1');
+            })
+            //var jsonfiledef = JSON.parse(test);
+            //console.log(cate);
+        */    
+        //console.log(cate);
+        //let unique = [...new Set(cate)];
+        //console.log(unique);
+        res.json(cate);
+    }
     catch (error) {
         res.send(error);
     }
@@ -105,14 +187,14 @@ router.get('/catalogo/productos/rango', async (req, res) => {
         precioMenor = catalogo[0].precio;
         precioMayor = catalogo[0].precio;
         console.log(precioMenor)
-        for (i = 0; i < catalogo.length; i++){
-            precio = catalogo[i].precio;
+        catalogo.forEach(element => {
+            precio = element.precio;
             if (precioMenor > precio){
                 precioMenor = precio;
             } else if (precioMayor < precio){
                 precioMayor = precio;
             }
-        } 
+        })
         //console.log(ret);
         console.log(precioMenor);
         console.log(precioMayor);
@@ -127,7 +209,7 @@ router.get('/catalogo/productos/rango', async (req, res) => {
 
 //localhost:4000/producto/precio?from=####&to=####
 //localhost:4000/productos/precio?from=####&to=####
-router.get('/catalogo/productos/precio', async (req, res) => {
+router.get('/catalogo/productos', async (req, res) => {
     console.log('its me again');
     var precioMenor = req.query.from;
     var precioMayor = req.query.to;
@@ -135,20 +217,20 @@ router.get('/catalogo/productos/precio', async (req, res) => {
     var ret = "";
     console.log(precioMenor, precioMayor);
     try{
-        for (i = 0; i < catalogo.length; i++){
-        if (catalogo[i].precio >= precioMenor && catalogo[i].precio <= precioMayor){
-            cont = cont + 1;
-            console.log(catalogo[i].idProducto);
-            //ret = ret + catalogo[i].idProducto;
-            var id = catalogo.idProducto;
-            const productos = await producto.find({idProducto: id});        
-            ret = ret + catalogo[i].idProducto + catalogo[i].categoria;
-            //ret = catalogo[i].idProducto + catalogo[i].categoria + catalogo[i].cantidadDisponible + catalogo[i].precio + catalogo[i].descripcion + productos[i].imagen + productos[i].miniatura
-            //catalogo[i].idProducto = productos.idProducto;
-            //catalogo[i].imagen = productos.imagen;
-            //catalogo[i].miniatura = productos.miniatura;
-        }         
-        } 
+        catalogo.forEach(element => {
+            if (element.precio >= precioMenor && element.precio <= precioMayor){
+                cont = cont + 1;
+                console.log(element.idProducto);
+                //ret = ret + catalogo[i].idProducto;
+                var id = element.idProducto;
+                //const productos = await producto.find({idProducto: id});        
+                ret = ret + catalogo[i].idProducto + catalogo[i].categoria;
+                //ret = catalogo[i].idProducto + catalogo[i].categoria + catalogo[i].cantidadDisponible + catalogo[i].precio + catalogo[i].descripcion + productos[i].imagen + productos[i].miniatura
+                //catalogo[i].idProducto = productos.idProducto;
+                //catalogo[i].imagen = productos.imagen;
+                //catalogo[i].miniatura = productos.miniatura;
+            }            
+        })
         console.log(ret);
         console.log(cont);
         res.json(catalogo);
@@ -163,28 +245,41 @@ router.get('/catalogo/productos/:id', async (req, res) => {
     //console.log(JSON.stringify(catalogo));
     try{
         var id = req.params.id;
-        var cont = 0;
+        //var cont = 0;
         console.log(id);
-            const productos = await producto.find({idProducto: id});
-            console.log(productos);
-            for (i = 0; i < catalogo.length; i++){
-                if (catalogo[i].idProducto == id){
-                    cont = i;
-                    break;
-                }
+        const productos = await producto.find({idProducto: id});
+        var data = {
+            idProducto: "",
+            categoria: "",
+            cantidadDisponible: "",
+            precio: "",
+            imagen: "",
+            miniatura: ""
+        };
+        catalogo.forEach(element => {
+            if (element.idProducto == id){
+                data = {
+                    idProducto: element.idProducto,
+                    categoria: element.categoria,
+                    cantidadDisponible: element.cantidadDisponible,
+                    precio: element.precio,
+                    imagen: productos[0].imagen,
+                    miniatura: productos[0].miniatura
+                };
             }
-            //console.log(catalogo[cont].idProducto)
-            var data = {
-                idProducto: catalogo[cont].idProducto,
-                categoria: catalogo[cont].categoria,
-                cantidadDisponible: catalogo[cont].cantidadDisponible,
-                precio: catalogo[cont].precio,
-                imagen: productos[0].imagen,
-                miniatura: productos[0].miniatura
-            };
-            res.json(data);      
-        }
-        
+        })
+            
+        //console.log(catalogo[cont].idProducto)
+        /*var data = {
+            idProducto: catalogo[cont].idProducto,
+            categoria: catalogo[cont].categoria,
+            cantidadDisponible: catalogo[cont].cantidadDisponible,
+            precio: catalogo[cont].precio,
+            imagen: productos[0].imagen,
+            miniatura: productos[0].miniatura
+        };*/
+        res.json(data);      
+    }        
     catch (error) {
         res.send(error);
     }
