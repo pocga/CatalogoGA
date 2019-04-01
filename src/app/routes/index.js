12a1640c2e19076f9137ce5e7f4421255d99a5d5
Sebastian Alarcon <sebastianalarcon@sophossolutions.com>
2019-04-01 16:37:01 -0500

    respuestas en JSON

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -20,7 +20,6 @@ client.on('connect', function() {
 //localhost:4000/catalogo/catalogo/productos?disp=true/false -> Consulta por disponibilidad
 router.get('/catalogo/productos/', async (req, res) => {
     
-    const sendresponse = option => res.end(option);
     precioMenor = req.query.from ? req.query.from : 0;
     precioMayor= req.query.to ? req.query.to :0;
     categ = req.query.categ ? req.query.categ :0; 
@@ -38,9 +37,12 @@ router.get('/catalogo/productos/', async (req, res) => {
             }
             if (redisFilter !== null){
                 valor = false;
-                sendresponse(redisFilter);
+                res.send(JSON.parse(redisFilter));
             }
         });
+
+        if (valor == false)
+            return;
 
         if (valor === true)
         {
@@ -95,7 +97,7 @@ router.get('/catalogo/productos/', async (req, res) => {
             productsList.producto = productsReturn;
             client.setex(rediskey, 30, JSON.stringify(productsList), redis.print);
             } 
-            sendresponse(JSON.stringify(productsList));
+            res.send(productsList);
                                   
         })
         .catch(function(err){
@@ -110,7 +112,7 @@ router.get('/catalogo/productos/', async (req, res) => {
 //localhost:4000/catalogo/productos/categorias/
 router.get('/catalogo/productos/categorias', async (req, res) => {
     
-    const sendresponse = option => res.end(option);
+    //const sendresponse = option => res.end(JSON.parse(JSON.stringify(option)));
     var valor=true;
 
     try{
@@ -121,9 +123,12 @@ router.get('/catalogo/productos/categorias', async (req, res) => {
             }
             if (redisCategory !== null){                
                 valor = false;
-                sendresponse(redisCategory);            
+                res.send(JSON.parse(redisCategory));            
             }
         });
+
+        if(valor === false)
+            return;
 
         if (valor===true)
         {    
@@ -133,7 +138,7 @@ router.get('/catalogo/productos/categorias', async (req, res) => {
                 })
                 .then(mockProductos=>{
                     client.setex('/catalogo/productos/categorias', 30, JSON.stringify(mockProductos), redis.print);
-                    sendresponse(JSON.stringify(mockProductos));
+                    res.send(mockProductos);
                 })
                 .catch(function(err){
                     console.log(err);
@@ -148,7 +153,6 @@ router.get('/catalogo/productos/categorias', async (req, res) => {
 //localhost:4000/catalogo/productos/rango/
 router.get('/catalogo/productos/rango', async (req, res) => {
 
-    const sendresponse = option => res.end(option);
     var precio;
     var precioMenor;
     var precioMayor;
@@ -163,9 +167,12 @@ router.get('/catalogo/productos/rango', async (req, res) => {
             }
             if (redisRange !== null){
                 valor = false;
-                sendresponse(redisRange);
+                res.send(JSON.parse(redisRange));
             }
         });
+
+        if(valor===false)
+            return;
 
         if (valor === true) {
             var range = [];
@@ -185,7 +192,7 @@ router.get('/catalogo/productos/rango', async (req, res) => {
                 })
                 range = {precioMenor, precioMayor};
                 client.setex('/catalogo/productos/rango', 30, JSON.stringify(range), redis.print);
-                sendresponse(JSON.stringify(range));
+                res.send(range);
             })
             .catch(function(err){
                 console.log(err);
@@ -193,15 +200,13 @@ router.get('/catalogo/productos/rango', async (req, res) => {
         }
 
     } catch (error) {
-        sendresponse(error);
+        res.semd(error);
     }
 });
 
 //localhost:4000/alogo/productos/""""
 router.get('/catalogo/productos/:id', async (req, res) => {
     
-    var validar = 0;
-
     try{
         let id = req.params.id;
         const productos = await producto.find({idProducto: id});
